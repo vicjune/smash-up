d0c9b93541ae84bd664cf4678675c187dfb10014
vicjune <victor.jouin@gmail.com>
2017-12-26 22:24:29 +0100

    Add shared module and basic components

---
+++ src/app/shared/components/baseList/baseList.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-base-list',
+  templateUrl: './baseList.component.html',
+  styleUrls: ['./baseList.component.scss']
+})
+export class BaseListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/shared/components/playerList/playerList.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-player-list',
+  templateUrl: './playerList.component.html',
+  styleUrls: ['./playerList.component.scss']
+})
+export class PlayerListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
